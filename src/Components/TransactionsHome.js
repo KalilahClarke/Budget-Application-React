da00081d81e91258c3328fb829c75bcf0957f740
Kalilah Clarke <kalilah.clarke@gmail.com>
2022-08-01 22:37:55 -0400

    Home Page addition button

--- src/Components/TransactionsHome.js
+++ src/Components/TransactionsHome.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import './TransactionsHome.css'
+import { useNavigate } from 'react-router-dom'
 
 export default function TransactionsHome() {
+  const navigate = useNavigate()
   return (
     <div>
       <h1>
-        Welcome to Your Budgeting Application 
+        Welcome to <br></br>Your <br></br> Budgeting Application 
         <br></br>
         Let's get Budgeting
         </h1>
+        <button onClick={()=>navigate('/transactions')}>Let's Get Started</button>
     </div>
   )
 }
